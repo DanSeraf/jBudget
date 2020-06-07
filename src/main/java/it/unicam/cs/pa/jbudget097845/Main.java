3570c2ecad03928dd150adcc4115b8ece72f1d3c
dseraf <me@danieleserafini.eu>
2020-06-08 00:31:29 +0200

    unused imports

--- src/main/java/it/unicam/cs/pa/jbudget097845/Main.java
+++ src/main/java/it/unicam/cs/pa/jbudget097845/Main.java
@@ -1,29 +1,11 @@
 package it.unicam.cs.pa.jbudget097845;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import it.unicam.cs.pa.jbudget097845.core.*;
-import it.unicam.cs.pa.jbudget097845.core.account.Account;
-import it.unicam.cs.pa.jbudget097845.core.account.AccountType;
-import it.unicam.cs.pa.jbudget097845.core.movement.Movement;
-import it.unicam.cs.pa.jbudget097845.core.movement.MovementManager;
-import it.unicam.cs.pa.jbudget097845.core.movement.MovementType;
-import it.unicam.cs.pa.jbudget097845.core.transaction.Transaction;
-import it.unicam.cs.pa.jbudget097845.core.transaction.TransactionManager;
 import it.unicam.cs.pa.jbudget097845.server.Server;
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Option;
-
-import java.io.File;
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         CommandLineParser.init();
         CommandLine cmd = CommandLineParser.getCommands(args);
         String host = cmd.getOptionValue("host") != null ? cmd.getOptionValue("host") : "0.0.0.0";
